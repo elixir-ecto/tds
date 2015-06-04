3b493d7ad8404bc0aecb1073ba40c545c9dd929f
Justin Schneck <jschneck@mac.com>
2015-06-04 10:59:52 -0400

    Set internal socket buffer

--- lib/tds/connection.ex
+++ lib/tds/connection.ex
@@ -130,6 +130,14 @@ defmodule Tds.Connection do
     case :gen_tcp.connect(host, port, sock_opts, timeout) do
       {:ok, sock} ->
         s = put_in s.sock, {:gen_tcp, sock}
+        # A suitable :buffer is only set if :recbuf is included in
+        # :socket_options.
+        {:ok, [sndbuf: sndbuf, recbuf: recbuf, buffer: buffer]} =
+          :inet.getopts(sock, [:sndbuf, :recbuf, :buffer])
+        buffer = buffer
+          |> max(sndbuf)
+          |> max(recbuf)
+        :ok = :inet.setopts(sock, [buffer: buffer, active: :once])
         Protocol.login(%{s | opts: opts, sock: {:gen_tcp, sock}})
       {:error, error} ->
         error(%Tds.Error{message: "tcp connect: #{error}"}, s)
