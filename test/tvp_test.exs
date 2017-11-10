6a93a1ff458232430b0e9c888c2c5a05ffdde538
Simon Thörnqvist <simon@plug-n-trade.com>
2017-11-10 10:41:52 +0100

    tvp test

---
+++ test/tvp_test.exs
@@ -0,0 +1,36 @@
+defmodule TvpTest do
+  import Tds.TestHelper
+  require Logger
+  use ExUnit.Case, async: true
+  alias Tds.Parameter
+
+  @tag timeout: 50000
+
+  setup do
+    opts = Application.fetch_env!(:tds, :opts)
+    {:ok, pid} = Tds.start_link(opts)
+
+    {:ok, [pid: pid]}
+  end
+
+  test "TVP in stored proc", context do
+    assert :ok = query("BEGIN TRY DROP PROCEDURE __tvpTest DROP TYPE TvpTestType END TRY BEGIN CATCH END CATCH", [])
+    assert :ok = query("""
+      CREATE TYPE TvpTestType AS TABLE (
+        d int
+      );
+    """, [])
+    assert :ok = query("""
+    CREATE PROCEDURE __tvpTest (@tvp TvpTestType readonly)
+      AS BEGIN
+        select * from @tvp
+      END
+    """, [])
+
+    params = [
+      %Parameter{name: "@tvp", value: %{name: "TvpTestType", columns: [%Parameter{name: "d", type: :integer, value: nil}], rows: [[1]]}, type: :tvp}
+    ]
+
+    assert {:ok, res} = query("__tvpTest @tvp", params)
+  end
+end
