dc0ac39736efd94874fd106b917d9c923bd1ea30
Milan Jaric <milan.jaric@gmail.com>
2017-11-22 23:54:49 +0100

    v1.0.5

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Tds.Mixfile do
 
   def project do
     [ app: :tds,
-      version: "1.0.4",
+      version: "1.0.5",
       elixir: "~> 1.0",
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
