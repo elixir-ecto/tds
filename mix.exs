a50fe978650d9e9cfa1120846bc9bccfcbed4e2e
Milan Jaric <milan.jaric@gmail.com>
2017-12-02 23:50:26 +0100

    v1.0.9

--- mix.exs
+++ mix.exs
@@ -1,10 +1,11 @@
 defmodule Tds.Mixfile do
+  @moduledoc false
   use Mix.Project
 
   def project do
     [
       app: :tds,
-      version: "1.0.8",
+      version: "1.0.9",
       elixir: "~> 1.0",
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
@@ -27,22 +28,10 @@ defmodule Tds.Mixfile do
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger, :db_connection, :decimal]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [
       {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
