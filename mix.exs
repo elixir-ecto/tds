179d2032200af5babedafc0d35ff62d2bcf48a6c
Milan Jaric <milan.jaric@gmail.com>
2018-04-15 22:00:08 +0200

    bugfix: missing string length case for length between 2000 and 4000 chars

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Tds.Mixfile do
   def project do
     [
       app: :tds,
-      version: "1.0.17",
+      version: "1.0.18",
       elixir: "~> 1.0",
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
