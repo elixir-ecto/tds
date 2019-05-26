ed94551b2fbde0d1b17e005377dedc671275d81f
Milan Jaric <milan.jaric@gmail.com>
2019-05-26 19:49:05 +0200

    compile time warning cleanup

--- lib/tds/protocol.ex
+++ lib/tds/protocol.ex
@@ -305,7 +305,7 @@ defmodule Tds.Protocol do
         server =
           str
           |> String.split(";")
-          |> Enum.chunk(2)
+          |> Enum.chunk_every(2)
           |> Enum.reduce([], fn [k, v], acc ->
             k =
               k
