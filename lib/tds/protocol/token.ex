ad95469fb40ec987da7c93b1182a9045f70fd74d
Milan Jaric <milan.jaric@gmail.com>
2019-07-30 22:44:58 +0200

    fixing token calas resolution

--- lib/tds/protocol/token.ex
+++ lib/tds/protocol/token.ex
@@ -25,16 +25,21 @@ defmodule Tds.Protocol.Token do
   #   {0xA4, :tab_name}
   # ]
 
-  def class(token) when token > 0 and token < 0xFF do
+  def class(token) when token > 0 and token <= 0xFF do
     case token &&& 0b00110000 do
       0b0000_0000 -> {:variable, :count}
-      0b0001_0000 -> {:fixed, 0}
       0b0010_0000 -> {:variable, :length}
-      0b0011_0000 -> {:fixed, 1}
-      0b0011_0100 -> {:fixed, 2}
-      0b0011_1000 -> {:fixed, 4}
-      0b0011_1100 -> {:fixed, 8}
+      0b0001_0000 -> {:fixed, 0}
+      0b0011_0000 -> {:fixed, fixed_length(token)}
     end
   end
 
+  defp fixed_length(token) do
+    case token &&& 0b00001100 do
+      0b0000_0000 -> 1
+      0b0000_0100 -> 2
+      0b0000_1000 -> 4
+      0b0000_1100 -> 8
+    end
+  end
 end
