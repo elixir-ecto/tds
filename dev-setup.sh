4e78954b38530c3d31d48d0f0fae7af1d9fb3665
whossname <tyson.buzza@gmail.com>
2020-01-12 14:41:09 +0800

    update db_connection version, setup for integration testing

---
+++ dev-setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# run in ecto_sql to set environment variables for development
+
+export TDS_PATH=../tds
+export ECTO_ADAPTER=mssql
--- docker-mssql.sh
+++ docker-mssql.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker run \
+  --name test_mssql_server
   -e 'ACCEPT_EULA=Y' \
   -e 'SA_PASSWORD=some!Password' \
   -p 1433:1433 \
