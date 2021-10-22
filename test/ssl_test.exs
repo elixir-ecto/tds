eb2bc25fcd4526a6dadb3fd6514b007f5ed6e965
kevin <mail@kseidel.org>
2021-10-22 15:15:38 +0200

    Add missing files and add test for non-ssl

--- test/ssl_test.exs
+++ test/ssl_test.exs
@@ -4,16 +4,33 @@ defmodule SSLTest do
 
   import Tds.TestHelper
 
-  setup do
-    opts = Application.fetch_env!(:tds, :opts)
-    opts = Keyword.put_new(opts, :ssl, true)
+  describe "test ssl connection" do
+    setup do
+      opts = Application.fetch_env!(:tds, :opts)
+      opts = Keyword.put_new(opts, :ssl, true)
 
-    {:ok, pid} = Tds.start_link(opts)
-    {:ok, [pid: pid]}
+      {:ok, pid} = Tds.start_link(opts)
+      {:ok, [pid: pid]}
+    end
+
+    test "open new ssl connection to database", context do
+      assert [["TRUE"]] ==
+               query("SELECT encrypt_option FROM sys.dm_exec_connections")
+    end
   end
 
-  test "open new ssl connection to database", context do
-    assert [["TRUE"]] ==
-             query("SELECT encrypt_option FROM sys.dm_exec_connections")
+  describe "test non-ssl connect" do
+    setup do
+      opts = Application.fetch_env!(:tds, :opts)
+      opts = Keyword.put_new(opts, :ssl, false)
+
+      {:ok, pid} = Tds.start_link(opts)
+      {:ok, [pid: pid]}
+    end
+
+    test "open new ssl connection to database", context do
+      assert [["FALSE"]] ==
+               query("SELECT encrypt_option FROM sys.dm_exec_connections")
+    end
   end
 end
