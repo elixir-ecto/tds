339b2ac9da029827549256b8d0055cd38778cca0
moogle <mail@kseidel.org>
2021-11-02 18:19:59 +0100

    Add missing file

---
+++ lib/tds/ucs2.ex
@@ -0,0 +1,25 @@
+defmodule Tds.UCS2 do
+  @moduledoc """
+  Converting UTF-8 strings into UCS-2 (UTF16 Little Endian) strings
+  used by the MSSQL Database
+  """
+  alias Tds.Utils
+
+  @ucs2_charset "utf-16le"
+
+  @doc """
+  Converts a UTF-8 string into UCS-2
+  """
+  @spec from_string(binary) :: binary
+  def from_string(string) when is_binary(string) do
+    Utils.encode_chars(string, @ucs2_charset)
+  end
+
+  @doc """
+  Converts a UCS-2 string into UTF-8
+  """
+  @spec to_string(binary) :: binary
+  def to_string(string) when is_binary(string) do
+    Utils.decode_chars(string, @ucs2_charset)
+  end
+end
