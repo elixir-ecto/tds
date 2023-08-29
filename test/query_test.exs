e7c59084bba31529e93d218f96738e530de47054
Simon McConnell <simonmcconnell@gmail.com>
2023-08-29 15:31:45 +0800

    implement `Table.Reader` for `Tds.Result`

--- test/query_test.exs
+++ test/query_test.exs
@@ -183,4 +183,20 @@ defmodule QueryTest do
              } = query("SELECT 1 WHERE 1 = @1", params, opts)
     end
   end
+
+  test "table reader integration", context do
+    assert {:ok, res} = query("SELECT * FROM (VALUES (1, 'a'), (2, 'b'), (3, 'c')) AS tab (x, y)", [])
+
+    assert res |> Table.to_rows() |> Enum.to_list() == [
+             %{"x" => 1, "y" => "a"},
+             %{"x" => 2, "y" => "b"},
+             %{"x" => 3, "y" => "c"}
+           ]
+
+    columns = Table.to_columns(res)
+    assert Enum.to_list(columns["x"]) == [1, 2, 3]
+    assert Enum.to_list(columns["y"]) == ["a", "b", "c"]
+
+    assert {_, %{count: 3}, _} = Table.Reader.init(res)
+  end
 end
